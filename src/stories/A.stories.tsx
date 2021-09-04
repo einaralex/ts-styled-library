ce6bbc0da9a91518a93ce226dfdf27d1951303b4
Einar Alexander Eymundsson @einar <einaralex@gmail.com>
2021-09-04 17:25:59 +0000

    using the root

--- src/stories/A.stories.tsx
+++ src/stories/A.stories.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { A } from '../a';
+import { A } from '../'
 
 export default {
   title: 'Example/A',
   component: A,
 } as ComponentMeta<typeof A>;
 
-const Template: ComponentStory<typeof A> = (args) => <A {...args} />;
+const Template: ComponentStory<any> = (args) => <A {...args} />;
 
 
 export const Default = Template.bind({});
--- src/stories/B.stories.tsx
+++ src/stories/B.stories.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import B from '../b';
+import { B } from '../';
 
 export default {
   title: 'Example/B',
   component: B,
 } as ComponentMeta<typeof B>;
 
-const Template: ComponentStory<typeof B> = (args) => <B {...args} />;
+// const Template: ComponentStory<typeof B> = (args) => <B {...args} />;
+const Template: ComponentStory<any> = (args) => <B {...args} />;
 
 
 export const Default = Template.bind({});
